0cd3a7b55a7fb216f4a02d7af8cda04fcd25dfb2
Jeon <wk0012345@gmail.com>
2018-05-13 12:07:07 +0900

    first commit

---
+++ priceServer.js
@@ -0,0 +1,51 @@
+/*
+
+var http = require('http'); 
+
+var server = http.createServer(function(request,response){ 
+
+    response.writeHead(200,{'Content-Type':'text/html'});
+    response.end("abc");
+
+});
+
+server.listen(8080, function(){ 
+    console.log('Server is running...');
+});
+
+
+*/
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+var result;
+
+var express = require('express');
+var app= express();
+app.listen(8080,function(){
+	console.log("start");
+});
+
+app.get('/',function(request,response){
+	response.sendFile('priceHome.html', { root: '.' })
+});
+
+app.post('/getPriceData',function(request,response){
+
+	MongoClient.connect(url,function(err,db){
+	if(err) throw err;
+	
+	var dbo= db.db("price");
+
+		dbo.collection("price").find().toArray(function(err,databaseResult){
+			if (err) throw err;
+		    console.log(databaseResult);
+
+		    result = databaseResult;
+
+
+		    db.close();
+		})
+	})
+		    console.log(result);
+	response.json(result);
+})
